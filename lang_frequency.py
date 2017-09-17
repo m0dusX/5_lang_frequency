a0322e9b987cc5bb6ebdba9def7e73134b5549bb
m0dusX <31633375+m0dusX@users.noreply.github.com>
2017-09-17 13:28:42 +0300

    Спасибо за проверку! Исправлено.
    Почему то решил использовать в предыдущем коде регулярные выражения. С модулем collections код сократился в три раза.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,44 +1,20 @@
 import sys
 import re
-from difflib import get_close_matches
-from operator import itemgetter
-from collections import OrderedDict
+from collections import Counter
 
 
-def load_data(filepath):
+def load_text_lowercase(filepath):
     with open(filepath, 'r', encoding="utf-8") as text:
         return text.read().lower()
 
 
 def get_most_frequent_words(text):
-    """Print list of most frequent words in loaded txt file.
-
-    Uses regex to find all words in text and writes them and their frequency 
-    to dictionary. Then it uses get_close_matches function from difflib to 
-    exclude words with same basis but with different endings, for e.g. 
-    dictionary and dictionaries, or "текст", "текста", "тексту" in russian.
-    Then it sorts dictionary and prints it to console.
-
-    """
     frequency = {}
-    match_pattern = re.findall(r'\w+', text)
-    for word in match_pattern:
-        counter = frequency.get(word, 0)
-        frequency[word] = counter + 1
-    value_list = frequency.keys()
-    for word in list(value_list):
-        matches = get_close_matches(word, value_list, n = 5, cutoff=0.8)
-        base_word = min(matches)
-        matches.remove(base_word)
-        for similar_word in matches:
-            frequency[base_word] += frequency.pop(similar_word)
-    sorted_tuple_list = sorted(frequency.items(), key=itemgetter(1), reverse=True)
-    del sorted_tuple_list[10:]
+    words= re.findall(r'\w+', text)
     print("Results: ")
-    for idx, word_tuple in enumerate(sorted_tuple_list, 1):
-        print("{}) {}: {}".format(idx, word_tuple[0], word_tuple[1]))
-        
+    for word in Counter(words).most_common(10):
+        print("{} : {}".format(word[0], word[1]))
 
 
 if __name__ == '__main__':
-    get_most_frequent_words(load_data(sys.argv[1]))
+    get_most_frequent_words(load_text_lowercase(sys.argv[1]))
